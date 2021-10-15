58db94ebbed7a25a1dd254b3fdeaebc39ce04145
Lionel Bonzoumet <lionel@declique.fr>
2021-10-15 17:20:39 +0200

    feat: add hero and testimonials

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Pricing from "./components/Pricing/Pricing";
 import Footer from "./components/Footer/Footer";
-import { ChakraProvider } from "@chakra-ui/react";
+import Hero from "./components/Hero/Hero";
+import Testimonials from "./components/Testimonials/Testimonials";
+import { ChakraProvider, Image } from "@chakra-ui/react";
 
 function App() {
   return (
     <ChakraProvider>
       <div className="App">
+        <Image src="/logo-square.png" alt="" mx="auto" />
+        <Hero />
         <Pricing />
+        <Testimonials />
         <Footer />
       </div>
     </ChakraProvider>
